75cf01efa4087420b1d3757e2cf76c92c14f8512
CarminaBJ <carmimamimi@hotmail.com>
2016-11-29 16:18:59 -0600

    Change the database

--- R/getAttr.R
+++ R/getAttr.R
@@ -17,48 +17,48 @@ getAttr<-function(attributes=NULL, filters=NULL, values=NULL, dataset=NULL, oper
   if(!all(dataset %in% listDatasets())){ #
     stop("The dataset you use dont exist in the database. Check function listDatasets()",call.= FALSE)
   }
-  
+
   #Validate attributes
   if(!all(attributes %in% listAttributes(dataset))){
     stop("The attributes to be retrieved do not exist. Please check listAttributes() function.",call.= FALSE)
   }
-  
-  #Validate if filters and values are the same size. Note: Also validates if either filters or values are null. 
-  if(length(filters)!=length(values)){ 
+
+  #Validate if filters and values are the same size. Note: Also validates if either filters or values are null.
+  if(length(filters)!=length(values)){
     stop("Parameters 'filters' and 'values' must be vectors of the same length.",call.= FALSE)
   }
-  
+
   #Make query
   if(is.null(filters)){
     query<-paste("SELECT ", paste(attributes, collapse=" , ")," FROM ", dataset, "; ",sep="")
   } else {
-    
+
     #Validate filters
     if(!all(filters %in% listAttributes(dataset))){
       stop("The filters do not exist. Please check listAttributes() function.",call.= FALSE)}
-    
+
     #Make Query
-    query <- paste(" ",filters[1]," = '",values[1],"'", sep="") 
+    query <- paste(" ",filters[1]," = '",values[1],"'", sep="")
     if(length(filters) > 1){
       query<-c(query, apply(cbind(filters,values),1,function(x){ # Is x a meaningful name?
-        result<-paste(" ", x[1], " = '", x[2],"\"",sep="") # x can be either a filter: x[1] or a value: x[2]  
+        result<-paste(" ", x[1], " = '", x[2],"\"",sep="") # x can be either a filter: x[1] or a value: x[2]
         return(result)
       })
       )
       query<-paste(query,collapse=operator)
     }
     query<-paste("SELECT ", paste(attributes, collapse=" , ")," FROM ", dataset, " WHERE ", query, sep="") #Construct query
-    
+
   }
-  
+
   #Connect to database
-  regulon<-RSQLite::dbConnect(RSQLite::SQLite(), system.file("extdata", "regulondb_92_sqlite3.db", package = "regutools"))
-  
+  regulon<-RSQLite::dbConnect(RSQLite::SQLite(), system.file("extdata", "regulondb_sqlite3.db", package = "regutools"))
+
   #Retrieve data
   result <-RSQLite::dbGetQuery(regulon, query)
   RSQLite::dbDisconnect(regulon)
-  
-  #Check if results are empty 
+
+  #Check if results are empty
   if(!nrow(result)){
     stop("Your query produced no results. Try changing the filters, values or attributes to be retrieved.",call.= FALSE)
   }
