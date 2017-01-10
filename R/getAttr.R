88f0baee2d68320eca116538ed55e63cb5b84f7e
Jose Alquicira H <joseah@lcg.unam.mx>
2017-01-10 16:33:41 -0600

    fixes list of parameters; fixes style

--- R/getAttr.R
+++ R/getAttr.R
@@ -1,53 +1,54 @@
 #' Get Attributes
 #'
-#' Retrieve attributes from datasets.
-#' @param
-#' attributes Attributes to be retrieved.  Defaults to NULL.
-#' filters   Filters to be used.  Defaults to NULL.
-#' values Values to filter with.  Defaults to NULL.
-#' dataset Dataset to retrieve from. Defaults to NULL.
-#' operator  Logical operator for the filters: AND / OR.  Defaults to AND.
-#' @keywords data retrieval, attributes,
+#' Retrieve information based on attributes and filters from datasets.
+#' @param attributes Attributes to be retrieved.
+#' @param filters   Filters to be used.
+#' @param values Values to filter with.
+#' @param dataset Dataset to retrieve from.
+#' @param operator  Logical operator for the filters: AND / OR.  Defaults to AND.
+#' @keywords data retrieval, attributes, filters,
 #' @export
+#' @author 
+#' Carmina Barberena Jonás, Jesús Emiliano Sotelo Fonseca, José Alquicira Hernández
 #' @examples
 #' getAttr(attributes=c("gene_id"), dataset="GENE")
 
-getAttr<-function(attributes=NULL, filters=NULL, values=NULL, dataset=NULL, operator='AND'){
+getAttr <- function(attributes = NULL, filters = NULL, values = NULL, dataset = NULL, operator = 'AND'){
   #Validate dataset
   if(!all(dataset %in% listDatasets())){ #
-    stop("The dataset you use dont exist in the database. Check function listDatasets()",call.= FALSE)
+    stop("The dataset you use dont exist in the database. Check function listDatasets()", call.= FALSE)
   }
 
   #Validate attributes
   if(!all(attributes %in% listAttributes(dataset))){
-    stop("The attributes to be retrieved do not exist. Please check listAttributes() function.",call.= FALSE)
+    stop("The attributes to be retrieved do not exist. Please check listAttributes() function.", call.= FALSE)
   }
 
   #Validate if filters and values are the same size. Note: Also validates if either filters or values are null.
-  if(length(filters)!=length(values)){
-    stop("Parameters 'filters' and 'values' must be vectors of the same length.",call.= FALSE)
+  if(length(filters) != length(values)){
+    stop("Parameters 'filters' and 'values' must be vectors of the same length.", call.= FALSE)
   }
 
   #Make query
   if(is.null(filters)){
-    query<-paste("SELECT ", paste(attributes, collapse=" , ")," FROM ", dataset, "; ",sep="")
+    query <- paste("SELECT ", paste(attributes, collapse=" , ")," FROM ", dataset, "; ",sep = "")
   } else {
 
     #Validate filters
     if(!all(filters %in% listAttributes(dataset))){
-      stop("The filters do not exist. Please check listAttributes() function.",call.= FALSE)}
+      stop("The filters do not exist. Please check listAttributes() function.", call.= FALSE)}
 
     #Make Query
     query <- paste(" ",filters[1]," = '",values[1],"'", sep="")
     if(length(filters) > 1){
-      query<-c(query, apply(cbind(filters,values),1,function(x){ # Is x a meaningful name?
-        result<-paste(" ", x[1], " = '", x[2],"\"",sep="") # x can be either a filter: x[1] or a value: x[2]
+      query <- c(query, apply(cbind(filters,values), 1, function(x){ # Is x a meaningful name?
+        result <- paste(" ", x[1], " = '", x[2],"\"", sep = "") # x can be either a filter: x[1] or a value: x[2]
         return(result)
       })
       )
-      query<-paste(query,collapse=operator)
+      query <- paste(query,collapse=operator)
     }
-    query<-paste("SELECT ", paste(attributes, collapse=" , ")," FROM ", dataset, " WHERE ", query, sep="") #Construct query
+    query <- paste("SELECT ", paste(attributes, collapse = " , ")," FROM ", dataset, " WHERE ", query, sep = "") #Construct query
 
   }
 
