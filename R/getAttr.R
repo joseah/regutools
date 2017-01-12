e060f13e67c6e1d16811a8f2b1e4170fbe845e9d
Jose Alquicira H <joseah@lcg.unam.mx>
2017-01-11 21:59:20 -0600

    substitutes example; changes coding style; modifies error messages

--- R/getAttr.R
+++ R/getAttr.R
@@ -10,58 +10,64 @@
 #' @export
 #' @author 
 #' Carmina Barberena Jonás, Jesús Emiliano Sotelo Fonseca, José Alquicira Hernández
-#' @examples
-#' getAttr(attributes=c("gene_id"), dataset="GENE")
+#' @examples 
+#' # Retrieve all genes included in all operons 
+#' # regulated by CRP transcriptional factor.
+#' 
+#' getAttr(attributes = c("operon_name", "op_gene_names"), 
+#' filters = c("op_tf_names"), 
+#' values = "CRP", 
+#' dataset = "OPERON_DM")
 
 getAttr <- function(attributes = NULL, filters = NULL, values = NULL, dataset = NULL, operator = 'AND'){
-  #Validate dataset
+  # Validate dataset
   if(!all(dataset %in% listDatasets())){ #
-    stop("The dataset you use dont exist in the database. Check function listDatasets()", call.= FALSE)
+    stop("Non-existing dataset used. Please check listDatasets() function.", call.= FALSE)
   }
 
-  #Validate attributes
+  # Validate attributes
   if(!all(attributes %in% listAttributes(dataset))){
     stop("The attributes to be retrieved do not exist. Please check listAttributes() function.", call.= FALSE)
   }
 
-  #Validate if filters and values are the same size. Note: Also validates if either filters or values are null.
+  # Validate if filters and values are the same size. Note: Also validates if either filters or values are null.
   if(length(filters) != length(values)){
-    stop("Parameters 'filters' and 'values' must be vectors of the same length.", call.= FALSE)
+    stop("'filters' and 'values' must be vectors of the same length.", call.= FALSE)
   }
 
-  #Make query
+  # Query database
   if(is.null(filters)){
     query <- paste("SELECT ", paste(attributes, collapse=" , ")," FROM ", dataset, "; ",sep = "")
   } else {
 
-    #Validate filters
+    # Validate filters
     if(!all(filters %in% listAttributes(dataset))){
-      stop("The filters do not exist. Please check listAttributes() function.", call.= FALSE)}
+      stop("Provided filters do not exist. Please check listAttributes() function.", call.= FALSE)}
 
-    #Make Query
-    query <- paste(" ",filters[1]," = '",values[1],"'", sep="")
+    # Query database
+    query <- paste(" ", filters[1], " = '", values[1], "'", sep = "")
     if(length(filters) > 1){
-      query <- c(query, apply(cbind(filters,values), 1, function(x){ # Is x a meaningful name?
+      query <- c(query, apply(cbind(filters,values), 1, function(x){
         result <- paste(" ", x[1], " = '", x[2],"\"", sep = "") # x can be either a filter: x[1] or a value: x[2]
         return(result)
       })
       )
-      query <- paste(query,collapse=operator)
+      query <- paste(query, collapse = operator)
     }
-    query <- paste("SELECT ", paste(attributes, collapse = " , ")," FROM ", dataset, " WHERE ", query, sep = "") #Construct query
+    query <- paste("SELECT ", paste(attributes, collapse = " , "), " FROM ", dataset, " WHERE ", query, sep = "") #Construct query
 
   }
 
-  #Connect to database
+  # Connect to database
   regulon<-RSQLite::dbConnect(RSQLite::SQLite(), system.file("extdata", "regulondb_sqlite3.db", package = "regutools"))
 
-  #Retrieve data
-  result <-RSQLite::dbGetQuery(regulon, query)
+  # Retrieve data
+  result <- RSQLite::dbGetQuery(regulon, query)
   RSQLite::dbDisconnect(regulon)
 
-  #Check if results are empty
+  # Check if results exist
   if(!nrow(result)){
-    stop("Your query produced no results. Try changing the filters, values or attributes to be retrieved.",call.= FALSE)
+    stop("Your query produced no results. Try changing values, filters or attributes.", call.= FALSE)
   }
   return(result)
 }
