2cd9e2e92d86d28b11fadc6f2b10a585b6de832f
Jose Alquicira H <joseah@lcg.unam.mx>
2017-03-09 16:37:54 -0600

    subsets column_name field

--- R/BuildCondition.R
+++ R/BuildCondition.R
@@ -6,8 +6,8 @@
 BuildCondition<-function(filters, dataset, operator){
 if(class(filters) == "list"){
   #solo que si no corre
-  if(!all(names(filters) %in% ListAttributes(dataset))){
-    At_NotEx<-(names(filters) %in% ListAttributes(dataset))
+  if(!all(names(filters) %in% ListAttributes(dataset)[["column_name"]])){
+    At_NotEx<-(names(filters) %in% ListAttributes(dataset)[["column_name"]])
     Names_AtNotEx<- names(filters)[which(!At_NotEx)]
     stop("Provided filter(s) in the list ", Names_AtNotEx , " do not exist. Please check listAttributes() function.", call.= FALSE)
   }
