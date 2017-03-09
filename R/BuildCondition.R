0cec278310d22a1adc4e8f3016ff1d0c4febc38b
Jose Alquicira H <joseah@lcg.unam.mx>
2017-03-09 16:32:42 -0600

    uses ListAttributes function

--- R/BuildCondition.R
+++ R/BuildCondition.R
@@ -1,11 +1,13 @@
 #' @title Construct logical condition
-#' @description
+#' @description Bulds a condition
+#' @export
+
 
 BuildCondition<-function(filters, dataset, operator){
 if(class(filters) == "list"){
   #solo que si no corre
-  if(!all(names(filters) %in% regutools::listAttributes(dataset))){
-    At_NotEx<-(names(filters) %in% regutools::listAttributes(dataset))
+  if(!all(names(filters) %in% ListAttributes(dataset))){
+    At_NotEx<-(names(filters) %in% ListAttributes(dataset))
     Names_AtNotEx<- names(filters)[which(!At_NotEx)]
     stop("Provided filter(s) in the list ", Names_AtNotEx , " do not exist. Please check listAttributes() function.", call.= FALSE)
   }
