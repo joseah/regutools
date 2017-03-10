08633a1a9ac39fca99352d8099ffbb6b3c3c2e76
Jose Alquicira H <joseah@lcg.unam.mx>
2017-03-10 12:36:23 -0600

    fixes coding style; renames variables

--- R/BuildCondition.R
+++ R/BuildCondition.R
@@ -4,7 +4,7 @@
 #' Carmina Barberena Jonás, Jesús Emiliano Sotelo Fonseca, José Alquicira Hernández
 #' @param filters A list with values to restrict the query. The names of all elements in this list correspond to the attributes to bre retrieved.
 #' @param dataset A string containing the name of the dataset of interest
-#' @param operator A string indicading if all condition -\code{filters}- (AND) or some of them (OR) should be met
+#' @param operator A string indicading if all conditions -\code{filters}- (AND) or some of them (OR) should be met
 #' @return A string with a sql logical condition to query the dataset
 #' @examples
 #' BuildCondition(filters = list(network_type = "TF-GENE",
@@ -14,20 +14,20 @@
 #' @export
 
 
-BuildCondition<-function(filters, dataset, operator){
-if(class(filters) == "list"){
-  #solo que si no corre
-  if(!all(names(filters) %in% ListAttributes(dataset)[["column_name"]])){
-    At_NotEx<-(names(filters) %in% ListAttributes(dataset)[["column_name"]])
-    Names_AtNotEx<- names(filters)[which(!At_NotEx)]
-    stop("Provided filter(s) in the list ", Names_AtNotEx , " do not exist. Please check listAttributes() function.", call.= FALSE)
+BuildCondition <- function(filters, dataset, operator){
+  if(class(filters) == "list"){
+    if(!all(names(filters) %in% ListAttributes(dataset)[["column_name"]])){
+      At_NotEx <- names(filters) %in% ListAttributes(dataset)[["column_name"]]
+      Names_AtNotEx <- names(filters)[which(!At_NotEx)]
+      stop("Provided filter(s) in the list ", Names_AtNotEx ,
+           " do not exist. Please check ListAttributes() function.", call.= FALSE)
+    }
+    condition.format <- mapply(paste0, filters, "'", SIMPLIFY = FALSE)
+    condition.format <- mapply(paste, names(condition.format), t, sep = " = '" , SIMPLIFY = FALSE)
+    condition.format <- lapply(condition.format, function(x){
+      paste0("(", paste(x, collapse = " OR "), ")")
+    })
+    condition <- paste(unlist(condition.format), collapse = paste0(" ", operator, " "))
+    return(condition)
   }
-  t<-mapply(paste0, filters, "'", SIMPLIFY = FALSE)
-  tm<-mapply(paste, names(t), t, sep=" = '" , SIMPLIFY = FALSE)
-  tm3<-lapply(tm, function(x){
-        paste0("(", paste(x, collapse = " or "),")")
-        })
-  query<-paste(unlist(tm3), collapse =paste0(" ",operator, " "))
-  return(query)
-}
 }
