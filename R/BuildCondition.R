53190b7028c843092d7ecd75d3c4d75ba7ed15ea
Jose Alquicira H <joseah@lcg.unam.mx>
2017-03-10 10:21:20 -0600

    adds documentation

--- R/BuildCondition.R
+++ R/BuildCondition.R
@@ -1,5 +1,16 @@
-#' @title Construct logical condition
-#' @description Bulds a condition
+#' @title Constructs logical condition to query database
+#' @description Given a list of filters, this function builds a logical condition to query database
+#' @author
+#' Carmina Barberena Jonás, Jesús Emiliano Sotelo Fonseca, José Alquicira Hernández
+#' @param filters A list with values to restrict the query. The names of all elements in this list correspond to the attributes to bre retrieved.
+#' @param dataset A string containing the name of the dataset of interest
+#' @param operator A string indicading if all condition -\code{filters}- (AND) or some of them (OR) should be met
+#' @return A string with a sql logical condition to query the dataset
+#' @examples
+#' BuildCondition(filters = list(network_type = "TF-GENE",
+#'                          regulator_name = "AraC"),
+#'                           dataset = "NETWORK",
+#'                           operator = "AND")
 #' @export
 
 
