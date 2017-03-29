a73d546744f352b24e61909f5134cd26ed7bf156
Jesús Emiliano Sotelo Fonseca <emimemime@MacBook-Pro-de-Jesus-2.local>
2017-03-29 16:09:24 -0600

    Changed effect to +, -, +/- notation.

--- R/GetNetwork.R
+++ R/GetNetwork.R
@@ -1,7 +1,7 @@
 #' GetNetwork
 #'
 #' Return complete regulatory network.
-#' @param type tf-gene, tf-tf, gene-gene
+#' @param type TF-GENE, TF-TF, GENE-GENE
 #' @keywords regulation retrieval, tf, networks,
 #' @export
 #' @author
@@ -9,15 +9,22 @@
 #' @examples
 #' # Retrieve regulation of araC
 #'
-#' GetGeneRegulation(genes = c("araC"),
-#' format = "multirow",
-#' output.type = "TF")
+#' GetNetwork(type="TF-GENE")
+#'
+#'
+#'
+
 GetNetwork<-function(type="TF-GENE"){
   #Get Network
   network<-GetAttr(attributes=c("regulator_name","regulated_name","effect"),
                    filters=list("network_type"=c(type)),
                    dataset="NETWORK")
   colnames(network)<-c("regulator","gene","effect")
+
+  #Change effect to +, - and +/-
+  network$effect<-sub(pattern="activator",replacement="+",x=network$effect)
+  network$effect<-sub(pattern="repressor",replacement="-",x=network$effect)
+  network$effect<-sub(pattern="dual",replacement="+/-",x=network$effect)
 
   return(network)
 }
